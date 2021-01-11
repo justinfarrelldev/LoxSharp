8366f35d91c5772113d3be29b86fbdb7b3cfae7e
VortrusDev <justinfarrellgamedev@gmail.com>
2021-01-10 21:33:16 -0500

    Got to Regular Languages and Expressions

---
+++ Token.cs
@@ -0,0 +1,23 @@
+namespace LoxSharp
+{
+    class Token
+    {
+        TokenType type;
+        string lexeme;
+        object literal;
+        int line;
+
+        Token(TokenType type, string lexeme, object literal, int line)
+        {
+            this.type = type;
+            this.lexeme = lexeme;
+            this.literal = literal;
+            this.line = line;
+        }
+
+        public string toString()
+        {
+            return $"{type} {lexeme} {literal}";
+        }
+    }
+}
